b5da16c00930cc9bef3525312121bbdaa92863e8
filipwinters <filip.winters@telenet.be>
2026-05-17 22:47:48 +0200

    Update send-photostrip.js

--- api/send-photostrip.js
+++ api/send-photostrip.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   try {
     const { email, imageBase64 } = req.body;
 
-    if (!email || !imageBase64) {
+    if (!email) {
       return res.status(400).json({ error: "Missing email or image" });
     }
 
